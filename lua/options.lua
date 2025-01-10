c663f8813b836aeb35a2c7b46c6681e82ad9dbf3
Tord Aronsen <tord.aronsen@skatteetaten.no>
2025-01-10 15:49:41 +0100

    relative numbering

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,7 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.opt.number = true           -- Show the absolute number on the current line
+vim.opt.relativenumber = true   -- Enable relative numbering for other lines
+
