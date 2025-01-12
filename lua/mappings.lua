a6203b3560e5871c7d61a2244a78edc7ed3e6d88
Tord Aronsen <tordaro@gmail.com>
2025-01-13 00:16:41 +0100

    enable jumping on mac

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<C-9>", "<C-]>", { desc = "Jump to tag (remapped from Ctrl-9)" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
