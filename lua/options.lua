66063b9be7ea2cbfbe7f66256a835e9534760341
Tord Aronsen <tord.aronsen@skatteetaten.no>
2025-01-29 10:28:36 +0100

    some vim options

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,6 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
-vim.opt.number = true           -- Show the absolute number on the current line
-vim.opt.relativenumber = true   -- Enable relative numbering for other lines
-
+vim.opt.number = true -- Show the absolute number on the current line
+vim.opt.relativenumber = true -- Enable relative numbering for other lines
+vim.opt.scrolloff = 10 -- Some space at end of screen
